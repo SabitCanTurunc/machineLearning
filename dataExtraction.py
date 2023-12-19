cd9aa25332db93bd132221dac59e98ed0a5945aa
SabitCanTurunc <turuncsc@gmail.com>
2023-12-19 06:44:16 +0300

    more product data

--- dataExtraction.py
+++ dataExtraction.py
@@ -3,11 +3,12 @@ import requests
 import json
 
 base_link = "https://www.boyner.com.tr/search?q=ayakkab%C4%B1&page="
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
 
 all_product_links = []
 
-for i in range(1):
+for i in range(1, 31):
     link = base_link + str(i)
     r = requests.get(link, headers=headers)
     soup = BeautifulSoup(r.content, "html.parser")
@@ -18,11 +19,9 @@ for i in range(1):
         links = product_list_wrapper.find_all("a", href=True)
         all_product_links.extend([link['href'] for link in links])
 
-
 product_data = []
 
 for product_link in all_product_links:
-
     product_url = f"https://www.boyner.com.tr{product_link}"
     product_page = requests.get(product_url, headers=headers)
     product_soup = BeautifulSoup(product_page.content, "html.parser")
@@ -31,20 +30,27 @@ for product_link in all_product_links:
     brand = brand.text if brand else None
 
     tech_specs = product_soup.find_all("span", class_="product-information-card_value__jGcTJ")
+    tech_specs2 = product_soup.find_all("span", class_="title_subtitle__9USXk")
 
     taban = next((span.text for span in tech_specs if "taban" in span.text.lower()), None)
     ic_materyal = next((span.text for span in tech_specs if "iç materyal" in span.text.lower()), None)
     dis_materyal = next((span.text for span in tech_specs if "dış materyal" in span.text.lower()), None)
-    mensi = next((span.text for span in tech_specs if "menşei" in span.text.lower()), None)
+    mensei = next((span.text for span in tech_specs if "menşei" in span.text.lower()), None)
+    cinsiyet = next((("erkek" if "erkek" in span.text.lower() else "kadın") for span in tech_specs2 if
+                     "erkek" in span.text.lower() or "kadın" in span.text.lower()), None)
+    tip = next((("terlik" if "terlik" in span.text.lower() else "ayakkabı" if "ayakkabı" in span.text.lower() else "bot" if "bot" in span.text.lower() else None)
+               for span in tech_specs2 if
+               "terlik" in span.text.lower() or "ayakkabı" in span.text.lower() or "bot" in span.text.lower()), None)
 
     product_data.append({
-        "brand": brand,
+        "marka": brand,
         "taban": taban,
         "iç materyal": ic_materyal,
         "dış materyal": dis_materyal,
-        "menşei": mensi
+        "menşei": mensei,
+        "cinsiyet": cinsiyet,
+        "tip": tip
     })
 
 with open('data.json', 'w', encoding='utf-8') as f:
-  json.dump(product_data, f, ensure_ascii=False)
-
+    json.dump(product_data, f, ensure_ascii=False)
