4b8d9a20c977d94cbe956021298659bd9a15b3aa
SabitCanTurunc <turuncsc@gmail.com>
2023-12-19 07:07:42 +0300

    comments

--- dataExtraction.py
+++ dataExtraction.py
@@ -2,23 +2,26 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
+# Ürünlerin bulunduğu sayfa linkini
 base_link = "https://www.boyner.com.tr/search?q=ayakkab%C4%B1&page="
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
-
+# Tarayıcı bilgileri
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
+# Tüm ürün linklerini depolamak için boş bir liste
 all_product_links = []
-
+# 1'den 30'a kadar olan sayfalardaki ürün linkleri toplanıyor
 for i in range(1, 31):
     link = base_link + str(i)
     r = requests.get(link, headers=headers)
     soup = BeautifulSoup(r.content, "html.parser")
 
     product_list_wrapper = soup.find("div", class_="product-list_productItemListWrapper__yKb6p")
-
+    # Ürün listesi wrapper'ı bulunuyor
     if product_list_wrapper:
+        # Her bir ürün linki bulunup listeye ekleniyor
         links = product_list_wrapper.find_all("a", href=True)
         all_product_links.extend([link['href'] for link in links])
 
+# Ürün bilgilerini depolamak için boş bir liste
 product_data = []
 
 for product_link in all_product_links:
@@ -42,6 +45,7 @@ for product_link in all_product_links:
                for span in tech_specs2 if
                "terlik" in span.text.lower() or "ayakkabı" in span.text.lower() or "bot" in span.text.lower()), None)
 
+    # Her bir ürünün bilgileri JSON formatında listeye ekle
     product_data.append({
         "marka": brand,
         "taban": taban,
@@ -51,6 +55,6 @@ for product_link in all_product_links:
         "cinsiyet": cinsiyet,
         "tip": tip
     })
-
+# Elde edilen ürün bilgileri JSON dosyasına yazdır
 with open('data.json', 'w', encoding='utf-8') as f:
     json.dump(product_data, f, ensure_ascii=False)
